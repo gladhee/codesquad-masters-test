fd00afc43a9af1eed07696d29a92e003ccb86ebc
gladhee <gladhee@icloud.com>
2024-12-01 19:27:13 +0900

    refactor(OutputView): 출력 문구 format 지정

--- OutputView.java
+++ OutputView.java
@@ -1,19 +1,25 @@
 public class OutputView {
 
-    private static final String WINNING_NUMBERS_MESSAGE = "로또 당첨 숫자: ";
-    private static final String PLAYER_NUMBERS_MESSAGE = "플레이어의 숫자: ";
-    private static final String MATCH_COUNT_MESSAGE = "일치한 숫자 개수: ";
+    private static final String WINNING_NUMBERS_MESSAGE = "로또 당첨 숫자: %s";
+    private static final String PLAYER_NUMBERS_MESSAGE = "플레이어의 숫자: %s";
+    private static final String MATCH_COUNT_MESSAGE = "일치한 숫자 개수: %s개";
 
     public void printWinningNumbers(WinningNumbers winningNumbers) {
-        System.out.println(WINNING_NUMBERS_MESSAGE + winningNumbers);
+        String formattedMessage = String.format(WINNING_NUMBERS_MESSAGE, winningNumbers);
+
+        System.out.println(formattedMessage);
     }
 
     public void printPlayerNumbers(Lotto lotto) {
-        System.out.println(PLAYER_NUMBERS_MESSAGE + lotto);
+        String formattedMessage = String.format(PLAYER_NUMBERS_MESSAGE, lotto);
+
+        System.out.println(formattedMessage);
     }
 
     public void printMatchCount(int matchCount) {
-        System.out.println(MATCH_COUNT_MESSAGE + matchCount + "개");
+        String formattedMessage = String.format(MATCH_COUNT_MESSAGE, matchCount);
+
+        System.out.println(formattedMessage);
     }
 
     public void printMessage(String message) {
