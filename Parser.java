494ded3c623e4b3be5726eb8d12214df9c8ab4b6
gladhee <gladhee@icloud.com>
2024-12-01 16:34:49 +0900

    feat(Parser): 출력 형식에 맞춰 포메팅하는 메소드 작성
    formatNumbers(List<Integer> numbers): String

--- Parser.java
+++ Parser.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Parser {
 
@@ -20,6 +21,13 @@ public class Parser {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자를 입력해주세요.");
         }
+    }
+
+    public static String formatNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .sorted()
+                .map(String::valueOf)
+                .collect(Collectors.joining(", "));
     }
 
 }
