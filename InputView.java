c1cea815f9716228c0fe2b591d6dd7abd4d09ec7
gladhee <gladhee@icloud.com>
2024-12-01 16:58:55 +0900

    feat(InputView): 인내 메시지 로직 작성
    printInputGuideMessage(): void

--- InputView.java
+++ InputView.java
@@ -8,6 +8,10 @@ public class InputView {
         this.scanner = new Scanner(System.in);
     }
 
+    public void printInputGuideMessage() {
+        System.out.println("1~45 중 로또 번호를 여섯개 입력하세요.");
+    }
+
     public String inputNumbers() {
         return scanner.nextLine();
     }
