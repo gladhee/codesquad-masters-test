f4ea9640b3ac8389aff29de66dbbd99cbff273b0
gladhee <gladhee@icloud.com>
2024-12-01 19:28:24 +0900

    refactor(InputView): 안내 문구 상수화

--- InputView.java
+++ InputView.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class InputView {
 
     private final Scanner scanner;
+    private static final String INPUT_GUIDE_MESSAGE = "1~45 중 로또 번호를 여섯개 입력하세요.";
 
     public InputView() {
         this.scanner = new Scanner(System.in);
     }
 
     public void printInputGuideMessage() {
-        System.out.println("1~45 중 로또 번호를 여섯개 입력하세요.");
+        System.out.println(INPUT_GUIDE_MESSAGE);
     }
 
     public String inputNumbers() {
