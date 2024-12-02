7018c864abc82f842a07aa3e1babcfc9f8498cc6
gladhee <gladhee@icloud.com>
2024-12-03 00:20:44 +0900

    refactor(InputView): 안내 문구 쉼표 구분 안내 추가

--- InputView.java
+++ InputView.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class InputView {
 
     private final Scanner scanner;
-    private static final String INPUT_GUIDE_MESSAGE = "1~45 중 로또 번호를 여섯개 입력하세요.";
+    private static final String INPUT_GUIDE_MESSAGE = "플레이어는 1~45 사이 로또 번호 여섯개를 선택하세요 (쉼표로 구분)";
 
     public InputView() {
         this.scanner = new Scanner(System.in);
