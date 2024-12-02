d91ac8c826162ec002097987b6e9051dca0103ab
gladhee <gladhee@icloud.com>
2024-12-02 19:49:24 +0900

    feat(GameMoney): 플레이어 재산 관리 클래스 구현
    - canPlay(): boolean
    메소드를 통해 로또 게임 진행이 가능한지 확인
    
    - deductGameCost(), addPrize() 를 통해 게임 진행

---
+++ GameMoney.java
@@ -0,0 +1,27 @@
+public class GameMoney {
+
+    private static final int GAME_COST = 1_000;
+    private static final int INITIAL_BALANCE = 10_000;
+    private int balance;
+
+    public GameMoney() {
+        this.balance = INITIAL_BALANCE;
+    }
+
+    public boolean canPlay() {
+        return balance >= GAME_COST;
+    }
+
+    public void deductGameCost() {
+        balance -= GAME_COST;
+    }
+
+    public void addPrize(int prizeMoney) {
+        balance += prizeMoney;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+}
