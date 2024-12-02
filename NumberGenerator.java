debbbebd53278331c644987e80f05ee31651e070
gladhee <gladhee@icloud.com>
2024-12-02 13:09:14 +0900

    feat(NumberGenerator): 보너스 번호 생성 로직 구현
    - generateBonusNumber(List<Integer> excludedNumbers): int
    매개 변수로 주어진 생성된 당첨 번호 제외한 숫자 하나를 랜덤으로 생성

--- NumberGenerator.java
+++ NumberGenerator.java
@@ -5,8 +5,12 @@ import java.util.List;
 import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Stream;
 
 public class NumberGenerator {
+
+    private static final Random random = ThreadLocalRandom.current();
+    private static final String UNEXPECTED_ERROR_MESSAGE = "예기치 못한 오류가 발생했습니다.";
 
     public static List<Integer> generateSortedUniqueNumbers() {
         Set<Integer> uniqueNumbers = getUniqueNumbers();
@@ -19,15 +23,25 @@ public class NumberGenerator {
 
     private static Set<Integer> getUniqueNumbers() {
         Set<Integer> uniqueNumbers = new HashSet<>();
-        Random random = ThreadLocalRandom.current();
 
         while (uniqueNumbers.size() < LottoConstants.REQUIRED_NUMBER_COUNT.getValue()) {
-            int number = random.nextInt(LottoConstants.MAX_NUMBER.getValue() - LottoConstants.MIN_NUMBER.getValue() + 1)
-                    + LottoConstants.MIN_NUMBER.getValue();
+            int number = pickRandomNumber();
             uniqueNumbers.add(number);
         }
 
         return uniqueNumbers;
+    }
+
+    public static int generateBonusNumber(List<Integer> excludedNumbers) {
+        return Stream.generate(NumberGenerator::pickRandomNumber)
+                .filter(number -> !excludedNumbers.contains(number))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException(UNEXPECTED_ERROR_MESSAGE));
+    }
+
+    private static int pickRandomNumber() {
+        return random.nextInt(LottoConstants.MAX_NUMBER.getValue() - LottoConstants.MIN_NUMBER.getValue() + 1)
+                + LottoConstants.MIN_NUMBER.getValue();
     }
 
 }
