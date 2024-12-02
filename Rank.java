ba70f2ff0830e8ddaa3a0e1b70fb5386ba76793d
gladhee <gladhee@icloud.com>
2024-12-02 19:41:33 +0900

    feat(Rank): 등수 별 상금 추가
    - 등수별로 알맞은 상금 금액 추가

--- Rank.java
+++ Rank.java
@@ -2,21 +2,23 @@ import java.util.Arrays;
 
 public enum Rank {
 
-    SIX_MATCHES(6, false),
-    FIVE_MATCHES_WITH_BONUS(5, true),
-    FIVE_MATCHES(5, false),
-    FOUR_MATCHES(4, false),
-    THREE_MATCHES(3, false),
-    TWO_MATCHES(2, false),
-    ONE_MATCH(1, false),
-    NO_MATCH(0, false);
+    SIX_MATCHES(6, false, 1_000_000),
+    FIVE_MATCHES_WITH_BONUS(5, true, 100_000),
+    FIVE_MATCHES(5, false, 10_000),
+    FOUR_MATCHES(4, false, 5_000),
+    THREE_MATCHES(3, false, 1_000),
+    TWO_MATCHES(2, false, 0),
+    ONE_MATCH(1, false, 0),
+    NO_MATCH(0, false, 0);
 
     private final int requiredMatchCount;
     private final boolean requiresBonusMatch;
+    private final int prize;
 
-    Rank(int requiredMatchCount, boolean requiresBonusMatch) {
+    Rank(int requiredMatchCount, boolean requiresBonusMatch, int prize) {
         this.requiredMatchCount = requiredMatchCount;
         this.requiresBonusMatch = requiresBonusMatch;
+        this.prize = prize;
     }
 
     public static Rank valueOf(int matchCount, boolean bonusMatch) {
@@ -31,6 +33,10 @@ public enum Rank {
             return matchCount == this.requiredMatchCount && bonusMatch;
         }
         return matchCount == this.requiredMatchCount;
+    }
+
+    public int getPrize() {
+        return prize;
     }
 
 }
